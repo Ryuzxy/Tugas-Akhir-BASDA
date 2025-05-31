acde5af0804fcf7ee62139d6a998205b1d9d2bba
Ryuzxy <thelavtovois@gmail.com>
2025-06-01 04:51:27 +0700

    lupa

--- MainProgam.py
+++ MainProgam.py
@@ -10,15 +10,15 @@ def animasi_teks(teks, delay=0.05):
     print()  
 
 def main():
-    # os.system('cls' if os.name == 'nt' else 'clear')
-    # judul = pyfiglet.figlet_format("SIMANDESA")
-    # for baris in judul.splitlines():
-    #     animasi_teks(baris, delay=0.01)
-    # animasi_teks("Sistem Informasi Desa", delay=0.04)
-    # animasi_teks("Selamat Datang di Sistem Informasi Desa Suci", delay=0.04)
-    # animasi_teks("Dibuat Dengan Cinta Oleh Kelompok 3", delay=0.04)
-    # animasi_teks("Silakan login untuk melanjutkan...", delay=0.04)
-    # time.sleep(2)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    judul = pyfiglet.figlet_format("SIMANDESA")
+    for baris in judul.splitlines():
+        animasi_teks(baris, delay=0.01)
+    animasi_teks("Sistem Informasi Desa", delay=0.04)
+    animasi_teks("Selamat Datang di Sistem Informasi Desa Suci", delay=0.04)
+    animasi_teks("Dibuat Dengan Cinta Oleh Kelompok 3", delay=0.04)
+    animasi_teks("Silakan login untuk melanjutkan...", delay=0.04)
+    time.sleep(2)
     LOGIN()
 
 main()
